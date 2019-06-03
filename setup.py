5d6b2b504c0a756ee47d4f1ee52ffcc24f7f1c9f
Sebastian <sebastian.mai@ovgu.de>
2019-06-03 14:47:30 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-experiment-runner',
+    version='0.1',
+    packages=['python-experiment-runner'],
+    url='https://github.com/ovgu-FINken/python-experiment-runner',
+    license='MIT',
+    author='Sebastian  Mai',
+    author_email='sebastian.mai@ovgu.de',
+    description='Running CS experiments with ipyparallel'
+)
